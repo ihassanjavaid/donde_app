171dc85fe80655466a242a9b6e4931ae46b3804d
Elvin Arrow <m.talhabinmansoor@gmail.com>
2020-03-14 15:12:17 +0500

    Update

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/cupertino.dart';
 
-const kWelcomeText = TextStyle(
+const kButtonContainerColour = 0xFFc4c4c4;
+
+const kWelcomeTextStyle = TextStyle(
   fontSize: 36.0,
   fontWeight: FontWeight.bold,
   color: Color(0xfff44336),
 );
 
-const kSubtitle = TextStyle(
+const kSubtitleStyle = TextStyle(
   fontSize: 20,
   color: Color(0xffcfcfcf),
+);
+
+//const kButtonStyle = BoxDecoration(
+//  borderRadius: BorderRadius.circular(10.0),
+//);
+
+const kButtonTextStyle = TextStyle(
+  color: Color(0xffffffff),
 );
