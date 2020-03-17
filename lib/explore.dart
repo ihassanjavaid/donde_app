c197370a20dd8066cfc77a2dc85c10a36151886e
Elvin Arrow <m.talhabinmansoor@gmail.com>
2020-03-17 19:38:10 +0500

    Added nearby restaurant feature

--- lib/explore.dart
+++ lib/explore.dart
@@ -2,6 +2,8 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Explore extends StatefulWidget {
   @override
@@ -11,6 +13,7 @@ class Explore extends StatefulWidget {
 class _ExploreState extends State<Explore> {
   // Attributes
   Completer<GoogleMapController> _controller = Completer();
+  Iterable markers = [];
 
   static final CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
@@ -26,15 +29,55 @@ class _ExploreState extends State<Explore> {
   void _getLocation() async {
     final GoogleMapController controller = await _controller.future;
 
+    // Get device current location
     Position position = await Geolocator()
         .getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
 
+    // Set restaurant markers around current locations
+    getPlaces(position);
+
+    // Set camera position at current position
     CameraPosition currentPosition = CameraPosition(
         bearing: 0,
         target: LatLng(position.latitude, position.longitude),
         zoom: 18.0);
 
     controller.animateCamera(CameraUpdate.newCameraPosition(currentPosition));
+  }
+
+  void getPlaces(Position position) async {
+    var lat = position.latitude;
+    var lng = position.longitude;
+    try {
+      final response = await http.get(
+          'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=$lat,$lng&radius=5000&type=restaurant&key=AIzaSyD8HBaKJrre0xJWU_vl81psl03U76rlIlI');
+
+      final int statusCode = response.statusCode;
+
+      if (statusCode == 201 || statusCode == 200) {
+        Map responseBody = json.decode(response.body);
+        List results = responseBody["results"];
+
+        Iterable _markers = Iterable.generate(results.length, (index) {
+          Map result = results[index];
+          Map location = result["geometry"]["location"];
+          LatLng latLngMarker = LatLng(location["lat"], location["lng"]);
+
+          return Marker(
+            markerId: MarkerId("marker$index"),
+            position: latLngMarker,
+          );
+        });
+
+        setState(() {
+          markers = _markers;
+        });
+      } else {
+        throw Exception('Error');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
   }
 
   @override
