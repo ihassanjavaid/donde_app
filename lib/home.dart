b2ad42334c7d8b15f83a2f511cca8858e2bc99bc
ihassanjavaid <great.maliks@gmail.com>
2020-03-17 16:20:42 +0500

    cosmetic changes & optimization

--- lib/home.dart
+++ lib/home.dart
@@ -49,54 +49,58 @@ class _HomeState extends State<Home> {
                 margin: EdgeInsets.symmetric(horizontal: 10.0),
                 height: 420.0,
                 width: double.infinity,
-                child: Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  // Card properties
-                  borderOnForeground: true,
-                  elevation: 18.0,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: <Widget>[
-                      Card(
-                        elevation: 8.5,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(6.5),
-                          child: Container(
-                            width: 330,
-                            height: 310,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.rectangle,
-                              image: DecorationImage(
-                                fit: BoxFit.fill,
-                                image: NetworkImage(
-                                    'https://www.thenews.com.pk//assets/uploads/akhbar/2019-04-22/461156_2369726_Islamabad-hotel_akhbar.jpg'),
+                child: GestureDetector(
+                  //this.onHorizontalDragDown
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    // Card properties
+                    borderOnForeground: true,
+                    elevation: 18.0,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Card(
+                          elevation: 10,
+                          borderOnForeground: true,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(6.5),
+                            child: Container(
+                              width: 330,
+                              height: 310,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.rectangle,
+                                image: DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image: NetworkImage(
+                                      'https://www.thenews.com.pk//assets/uploads/akhbar/2019-04-22/461156_2369726_Islamabad-hotel_akhbar.jpg'),
+                                ),
                               ),
                             ),
                           ),
                         ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          Text(
-                            'Restaurant Name',
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              fontSize: 24.0,
-                              fontWeight: FontWeight.normal,
-                              color: Colors.redAccent,
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Text(
+                              'Restaurant Name',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                fontSize: 28.0,
+                                fontWeight: FontWeight.normal,
+                                color: Colors.redAccent,
+                              ),
                             ),
-                          ),
-                          Text(
-                            '0.0 kms',
-                            textAlign: TextAlign.end,
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ],
+                            Text(
+                              '0.0 kms',
+                              textAlign: TextAlign.end,
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
