abb3c6e62cc185c8bc04418e3988209f5053e76e
Elvin Arrow <m.talhabinmansoor@gmail.com>
2020-03-17 13:10:22 +0500

    Create Divider with text

---
+++ lib/dividerWithText.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class DividerWithText extends StatelessWidget {
+  final String text;
+
+  DividerWithText({@required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Divider(
+            thickness: 1.0,
+            color: Color(0xffa9a9a9),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            this.text,
+            style: kNormalTextStyle,
+          ),
+        ),
+        Expanded(
+          child: Divider(
+            thickness: 1.0,
+            color: Color(0xffa9a9a9),
+          ),
+        ),
+      ],
+    );
+  }
+}
