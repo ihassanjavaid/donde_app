016d8fd6a9f90923b870dd40c2302ba810c62e95
elvin_arrow <m.talhabinmansoor@gmail.com>
2020-03-30 17:58:46 +0500

    Skeleton Added

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,10 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:donde/constants.dart';
+import 'package:donde/components/rounded_button.dart';
+import 'package:country_code_picker/country_code_picker.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   static const String id = 'login_screen';
 
   @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController controller;
+  Animation animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    animation =
+        CurvedAnimation(parent: this.controller, curve: Curves.bounceOut);
+    controller.forward();
+    controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 50.0,
+              ),
+              Text(
+                'Hello!',
+                style:
+                    kWelcomeTextStyle.copyWith(fontSize: animation.value * 48),
+              ),
+              AnimatedOpacity(
+                opacity: controller.value,
+                duration: Duration(milliseconds: 500),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      child: Text(
+                        'Get started, Enter your phone number',
+                        style: kSubtitleStyle,
+                        textAlign: TextAlign.left,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                    Container(
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            flex: 1,
+                            child: CountryCodePicker(
+                              initialSelection: 'pk',
+                              onInit: (countryCode) {},
+                              onChanged: (countryCode) {},
+                            ),
+                          ),
+                          Expanded(
+                            flex: 4,
+                            child: TextField(
+                              keyboardType: TextInputType.phone,
+                              onChanged: (value) {},
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.all(10.0),
+                                labelText: 'Enter Number',
+                                hasFloatingPlaceholder: true,
+                                labelStyle: TextStyle(
+                                  color: Colors.redAccent,
+                                ),
+                                border: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.redAccent,
+                                  ),
+                                ),
+                                focusedBorder: UnderlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: Colors.redAccent,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      margin: EdgeInsets.only(bottom: 10.0),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                      ),
+                    ),
+                    RoundedButton(
+                      buttonLabel: 'Next',
+                      onTap: () {},
+                      /*onTap: () {
+                          Navigator.pushNamed(context, Index.id);
+                        },*/
+                      colour: Color(kButtonContainerColour),
+                    ),
+                    SizedBox(
+                      height: 30.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -1,10 +1,195 @@
 import 'package:flutter/material.dart';
+import 'package:donde/components/custom_text_field.dart';
+import 'package:donde/screens/login_screen.dart';
 
 class RegistrationScreen extends StatelessWidget {
   static const String id = 'registration_screen';
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Stack(
+          children: <Widget>[
+            lowerHalf(context),
+            upperHalf(context),
+            signUpCard(context),
+            pageTitle(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget upperHalf(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height / 2,
+      child: Image.asset(
+        'images/food.jpeg',
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  Widget lowerHalf(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: MediaQuery.of(context).size.height / 2,
+        color: Color(0xFFECF0F3),
+      ),
+    );
+  }
+
+  Widget signUpCard(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          margin: EdgeInsets.only(top: MediaQuery.of(context).size.height / 5),
+          padding: EdgeInsets.only(left: 10, right: 10),
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            elevation: 18,
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      "CREATE ACCOUNT",
+                      style: TextStyle(
+                        color: Colors.redAccent,
+                        fontSize: 28,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  CustomTextField(
+                    placeholder: 'Your Name',
+                    onChanged: (value) {},
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  CustomTextField(
+                    placeholder: 'Your E-Mail',
+                    onChanged: (value) {},
+                    keyboardType: TextInputType.emailAddress,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  CustomTextField(
+                    placeholder: 'Your Password',
+                    isPassword: true,
+                    onChanged: (value) {},
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Password must be at least 8 characters and include a special character and number",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      Expanded(
+                        child: Container(),
+                      ),
+                      FlatButton(
+                        child: Text(
+                          "SIGN UP",
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        color: Colors.redAccent,
+                        textColor: Colors.white,
+                        padding: EdgeInsets.only(
+                            left: 38, right: 38, top: 15, bottom: 15),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        onPressed: () async {},
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              height: 40,
+            ),
+            Text(
+              "Already have an account?",
+              style: TextStyle(color: Colors.grey, fontSize: 15),
+            ),
+            FlatButton(
+              onPressed: () {
+                Navigator.popAndPushNamed(context, LoginScreen.id);
+              },
+              textColor: Colors.black87,
+              child: Text(
+                "LOGIN",
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+            )
+          ],
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: FlatButton(
+            child: Text(
+              "Terms & Conditions",
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget pageTitle() {
+    return Container(
+      margin: EdgeInsets.only(top: 50),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            Icons.fastfood,
+            size: 58,
+            color: Colors.white,
+          ),
+          Text(
+            "Donde",
+            style: TextStyle(
+                fontSize: 60,
+                color: Colors.white,
+                fontWeight: FontWeight.w400,
+                fontStyle: FontStyle.normal),
+          )
+        ],
+      ),
+    );
   }
 }
